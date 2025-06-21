e06981f82b8c35e424ebfcbda773e14b9c80a56e
sace771103 <37712788+sace771103@users.noreply.github.com>
2025-06-21 10:05:25 -0600

    Create index.ts

---
+++ src/engines/index.ts
@@ -0,0 +1,5 @@
+
+# ARCHIVO 8: src/engines/index.ts
+export { MIAHealthCalculationEngine as default } from './premium-calculation-engine.js';
+export * from './premium-calculation-engine.js';
+export * from './calculation-validator.js';
